fd88b314a862d25f040978cbbf56622487585e2d
Renato Takaoka <renato.takaoka13@gmail.com>
2024-08-12 11:05:49 -0300

    Second commit

--- ms-pagamento/src/main/java/com/github/rkkt/ms_pagamento/controller/PagamentoController.java
+++ ms-pagamento/src/main/java/com/github/rkkt/ms_pagamento/controller/PagamentoController.java
@@ -2,13 +2,16 @@ package com.github.rkkt.ms_pagamento.controller;
 
 import com.github.rkkt.ms_pagamento.dto.PagamentoDTO;
 import com.github.rkkt.ms_pagamento.service.PagamentoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,8 +22,8 @@ public class PagamentoController {
     private PagamentoService service;
 
     @GetMapping
-    public ResponseEntity<List<PagamentoDTO>> findAll() {
-        List<PagamentoDTO> dto = service.findAll();
+    public ResponseEntity<Page<PagamentoDTO>> findAll(@PageableDefault(size = 10)Pageable pageable) {
+        Page<PagamentoDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 
@@ -28,6 +31,22 @@ public class PagamentoController {
     public ResponseEntity<PagamentoDTO> findById(@PathVariable Long id) {
         PagamentoDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PagamentoDTO> insert(@RequestBody @Valid PagamentoDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- ms-pagamento/src/main/java/com/github/rkkt/ms_pagamento/service/PagamentoService.java
+++ ms-pagamento/src/main/java/com/github/rkkt/ms_pagamento/service/PagamentoService.java
@@ -3,13 +3,17 @@ package com.github.rkkt.ms_pagamento.service;
 
 import com.github.rkkt.ms_pagamento.dto.PagamentoDTO;
 import com.github.rkkt.ms_pagamento.model.Pagamento;
+import com.github.rkkt.ms_pagamento.model.Status;
 import com.github.rkkt.ms_pagamento.repository.PagamentoRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,18 +24,49 @@ public class PagamentoService {
     private PagamentoRepository repository;
 
     @Transactional(readOnly = true)
-    public List<PagamentoDTO> findAll() {
-        List<Pagamento> list = repository.findAll();
-        return list.stream().map(PagamentoDTO::new).collect(Collectors.toList());
+    public Page<PagamentoDTO> findAll(Pageable pageable) {
+        Page<Pagamento> page = repository.findAll(pageable);
+        return page.map(PagamentoDTO::new);
     }
 
     @Transactional(readOnly = true)
-    public PagamentoDTO findById(long id) {
+    public PagamentoDTO findById(Long id) {
         Pagamento entity = repository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Recurso não encontrado")
         );
 
         return new PagamentoDTO(entity);
+    }
+
+    @Transactional
+    public PagamentoDTO insert(PagamentoDTO dto) {
+        Pagamento entity = new Pagamento();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new PagamentoDTO(entity);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new EntityNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        } catch (EntityNotFoundException e) {
+            throw new EntityNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    private void copyDtoToEntity(PagamentoDTO dto, Pagamento entity) {
+        entity.setValor(dto.getValor());
+        entity.setNome(dto.getNome());
+        entity.setNumeroDoCartao(dto.getNumeroDoCartao());
+        entity.setValidade(dto.getValidade());
+        entity.setCodigoDeSeguranca(dto.getCodigoDeSeguranca());
+        entity.setStatus(Status.CRIADO);
+        entity.setPedidoId(dto.getPedidoId());
+        entity.setFormaDePagamentoId(dto.getFormaDePagamento());
     }
 
 }
